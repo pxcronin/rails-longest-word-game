4b30e7acf0efdfb4065b273040072b9e6302aecb
Patrick Cronin <pxcronin@gmail.com>
2020-11-10 09:00:55 -0500

    Final tweaks to push

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,3 @@
-require 'net/http'
 require 'open-uri'
 
 class GamesController < ApplicationController
@@ -14,7 +13,16 @@ class GamesController < ApplicationController
     @score = 0
     @letters_hash = Hash[params[:letters].split('').group_by{ |char| char }.map{ |key, val| [key, val.size] }]
     @word_hash = Hash[params[:word].split('').group_by{ |char| char }.map{ |key, val| [key, val.size] }]
-    @grid_check = (@word_hash.to_a - @letters_hash.to_a).empty?
+    # @grid_check = (@word_hash.to_a - @letters_hash.to_a).empty?
+    # @grid_check = @letters_hash.values_at(*@word_hash.keys) == @word_hash.values
+    @grid_check = @letters_hash.values_at(*@word_hash.keys)
+    if @grid_check.include?(nil)
+      @grid_check = false
+    elsif @grid_check.map { |count| count == @word_hash.values } #This is what's not working. It's comparing the total array of the count, not each individual one
+      @grid_check = true
+    else
+      @grid_check = false
+    end
     case 
     when word_json['found'] && @grid_check
       @score += params[:word].length * 3
